3c887d8c30c5faca8e97c4d30dd49638e7fc09ea
Adiul Alam Adil <adiulalam@gmail.com>
2022-09-06 00:28:44 +0100

    error page

--- views/error.html.php
+++ views/error.html.php
@@ -1,12 +1,33 @@
+<?php
+include_once $_SERVER['DOCUMENT_ROOT'] . '/connection/helpers.inc.php';
+include_once $_SERVER['DOCUMENT_ROOT'] . "/environment.php";
+include_once $_SERVER['DOCUMENT_ROOT'] . "/path.php";
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title>Error Page</title>
+    <meta charset="UTF-8">
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss/dist/tailwind.min.css" rel="stylesheet">
+    <link href="https://unpkg.com/tailwindcss@2.2.19/dist/tailwind.min.css" rel=" stylesheet">
+    <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
+    <meta charset="utf-8">
+    <title>Error Page</title>
 </head>
+
+<?php include_once $_SERVER['DOCUMENT_ROOT'] . '/components/nav.html.php'; ?>
+
 <body>
-    <p>
-        <?php echo $error; ?>
-    </p>
+    <section class="bg-gray-50 dark:bg-gray-900">
+        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+            <?php
+            generateAlert($error);
+            exit();
+            ?>
+        </div>
+    </section>
+
 </body>
+
 </html>
