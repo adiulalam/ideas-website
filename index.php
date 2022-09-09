3fe35c40d75813a226fb3fa3b2ccb2bf437fe0f2
Adiul Alam Adil <adiulalam@gmail.com>
2022-09-10 00:30:15 +0100

    add idea

--- index.php
+++ index.php
@@ -1,6 +1,132 @@
 <?php
 include_once $_SERVER['DOCUMENT_ROOT'] . "/path.php";
 
+
+if (isset($_GET['addform'])) {
+    include "$_PATH[databasePath]";
+
+
+    $text = $_POST['text'];
+    $Author = $_POST['Author'];
+    $Category = $_POST['categories'];
+    $Department = $_POST['departments'];
+
+
+    // $message = json_encode($clean);
+    // $message = json_encode($clean);
+    // generateAlert($message);
+    // echo "<script type='text/javascript'>alert('$message');</script>";
+
+    if ($text == '' || $Author == '' || $Category == '' || $Department == '') {
+        $error = 'Error: Field is empty';
+        include "$_PATH[errorPath]";
+        exit();
+    }
+
+    exit();
+
+    // if ($_POST['Author'] == '' || $_POST['text'] == '' || $_POST['Email'] == '') {
+    //     $error = 'You must choose an author or enter text for this idea, Click back and try again';
+    //     include 'error.html.php';
+    //     exit();
+    // }
+
+    /*****************************************************/
+    //Insert documents
+
+    // date time
+    $date = new DateTime();
+    $datetime = $date->format('-Y-m-d H:i:s');
+
+    // name of the uploaded file
+    $filename = $_FILES['myfile']['name'];
+
+    // destination of the file on the server
+    $destination = '../documents/';
+
+    // get the file extension
+    $extension = pathinfo($filename, PATHINFO_EXTENSION);
+
+    $filename =  str_replace('.' . $extension, '', $filename);
+
+    // the physical file on a temporary uploads directory on the server
+    $file = $_FILES['myfile']['tmp_name'];
+
+    //new file name with date
+    $Document = $filename . $datetime . '.' . $extension;
+
+    try {
+        move_uploaded_file($file, $destination . $Document);
+
+        require_once '../admin/includes/HTMLPurifier.standalone.php';
+        // $purifier = new HTMLPurifier();
+        // $clean = $purifier->purify($_POST['text']);
+        $clean = $_POST['text'];
+
+
+        $sql = 'INSERT INTO Idea SET
+        IdeaText=:IdeaText,
+        IdeaDate=CURDATE(),
+        AuthorID=:AuthorID,
+        Document=:Document';
+        $s = $pdo->prepare($sql);
+        $s->bindvalue(':IdeaText', $clean);
+        $s->bindvalue(':AuthorID', $_POST['Author']);
+        $s->bindvalue(':Document', $Document);
+        $s->execute();
+    } catch (PDOException $e) {
+        $error = 'Error adding submitted idea';
+        include 'error.html.php';
+        exit();
+    }
+
+    $IdeaID = $pdo->lastInsertId();
+
+
+    /*****************************************************/
+    //Insert record into ideacategory table --category
+
+    if (isset($_POST['categories'])) {
+        try {
+            $sql = 'INSERT INTO IdeaCategory SET
+        IdeaID=:IdeaID,
+        CategoryID=:CategoryID';
+            $s = $pdo->prepare($sql);
+            foreach ($_POST['categories'] as $CategoryID) {
+                $s->bindvalue(':IdeaID', $IdeaID);
+                $s->bindvalue(':CategoryID', $CategoryID);
+                $s->execute();
+            }
+        } catch (PDOException $e) {
+            $error = 'Error inserting idea into selected categories';
+            include 'error.html.php';
+            exit();
+        }
+    }
+
+    /*****************************************************/
+    //Insert record into ideadepartment table --department
+
+    if (isset($_POST['departments'])) {
+        try {
+            $sql = 'INSERT INTO IdeaDepartment SET
+        IdeaID=:IdeaID,
+        DepartmentID=:DepartmentID';
+            $s = $pdo->prepare($sql);
+
+            foreach ($_POST['departments'] as $DepartmentID) {
+                $s->bindvalue(':IdeaID', $IdeaID);
+                $s->bindvalue(':DepartmentID', $DepartmentID);
+                $s->execute();
+            }
+        } catch (PDOException $e) {
+            $error = 'Error inserting idea into selected departments';
+            include 'error.html.php';
+            exit();
+        }
+    }
+}
+
 if (isset($_GET['action']) and $_GET['action'] == 'addIdea') {
     include "$_PATH[databasePath]";
 
--- views/ideas/form.html.php
+++ views/ideas/form.html.php
@@ -25,29 +25,51 @@ if (!userIsLoggedIn()) {
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/components/nav.html.php'; ?>
 
 <body>
-    <section class="dark:bg-gray-900">
-        <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+    <section class=" dark:bg-gray-900">
+
+        <div class="flex flex-col my-auto items-center bgimg bg-cover p-6 py-6">
             <div class="w-full rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                 <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                     <h1 class="text-xl font-bold leading-tight tracking-tight md:text-2xl dark:text-white">
                         <?php html($pageTitle); ?>
                     </h1>
-                    <form class="space-y-4 md:space-y-6" action="?<?php html($action); ?>" method="POST" enctype="multipart/form-data">
+                    <form class="space-y-4 md:space-y-6" action="?<?php html($action); ?>" method="POST">
                         <div>
-                            <label for="email" class="block mb-2 text-sm font-medium dark:text-white">Your email</label>
-                            <input type="email" name="Email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="joedavis@domain.com" required>
+                            <label for="ideaText" class="block mb-2 text-sm font-medium dark:text-white">Your idea here:</label>
+                            <textarea name="text" rows="3" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your idea" required><?php html($text); ?></textarea>
+                            <input type="hidden" name="Author" value="<?php html($_SESSION['aid']); ?>">
                         </div>
-                        <div>
-                            <label for="password" class="block mb-2 text-sm font-medium dark:text-white">Password</label>
-                            <input type="password" name="Password" id="password" minlength="8" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required>
-                        </div>
-                        <!-- <div class=" flex flex-col items-center justify-center g-recaptcha" data-sitekey="6LfzRtIhAAAAAPlVMVWxaishNuL6inEyPsTIFSD6"></div> -->
-                        <input type="hidden" name="action" value="login">
-                        <button type="submit" class="modal-open w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login</button>
 
-                        <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                            Don't have an account? <a href="<?php echo '/views/auth/register/' ?>" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Regsiter here</a>
-                        </p>
+                        <fieldset class="rounded border border-gray-300 dark:border-gray-700 p-3">
+                            <legend class="block mb-2 text-sm font-medium dark:text-white">Categories:</legend>
+                            <ul class="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                <li class="w-full rounded-t-lg ">
+                                    <?php foreach ($categories as $Category) : ?>
+                                        <div class="flex items-center pl-3">
+                                            <input type="checkbox" name="categories[]" value="<?php html($Category['ID']); ?>" <?php if ($Category['selected']) echo ' checked' ?> class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="Category<?php html($Category['ID']); ?>" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"><?php html($Category['Name']); ?></label>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </li>
+                            </ul>
+                        </fieldset>
+
+                        <fieldset class="rounded border border-gray-300 dark:border-gray-700 p-3">
+                            <legend class="block mb-2 text-sm font-medium dark:text-white">Departments:</legend>
+                            <ul class="w-full text-sm font-medium text-gray-900 bg-white rounded-lg border border-gray-200 dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+                                <li class="w-full rounded-t-lg ">
+                                    <?php foreach ($departments as $Department) : ?>
+                                        <div class="flex items-center pl-3">
+                                            <input type="checkbox" name="departments[]" value="<?php html($Departmenty['ID']); ?>" <?php if ($Department['selected']) echo ' checked' ?> class="w-4 h-4 text-blue-600 bg-gray-100 rounded border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
+                                            <label for="Category<?php html($Department['ID']); ?>" class="py-3 ml-2 w-full text-sm font-medium text-gray-900 dark:text-gray-300"><?php html($Department['Name']); ?></label>
+                                        </div>
+                                    <?php endforeach; ?>
+                                </li>
+                            </ul>
+                        </fieldset>
+
+                        <input type="hidden" name="ID" value="<?php html($ID); ?>">
+                        <button type="submit" value="<?php html($button); ?>" class="modal-open w-full text-white bg-blue-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"><?php html($button); ?></button>
                     </form>
                 </div>
             </div>
