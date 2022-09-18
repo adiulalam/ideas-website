6cb35e35c11dde7a97e86253a5297f490646e7ba
Adiul Alam Adil <adiulalam@gmail.com>
2022-09-18 19:15:35 +0100

    cleanup

--- views/ideas/delete.php
+++ views/ideas/delete.php
@@ -6,54 +6,49 @@ function ideasDelete()
 
     $ideasID = $_POST['ID'];
 
-    //delete the idea
     try {
         session_start();
         $authorID =  $_SESSION['aid'];
         $sql = "DELETE FROM Idea WHERE ID = :ID AND AuthorID = :AuthorID";
-        // echo $sql;
         $s = $pdo->prepare($sql);
         $s->bindvalue(':ID', $ideasID);
         $s->bindvalue(':AuthorID', $authorID);
         $s->execute();
     } catch (PDOException $e) {
-        $error = 'Error deleting idea';
+        $error = 'Error deleting idea from idea';
         include "$_PATH[errorPath]";
         exit();
     }
 
-    //delete the idea category
     try {
         $sql = "DELETE FROM IdeaCategory WHERE IdeaID= :ID";
         $s = $pdo->prepare($sql);
         $s->bindvalue(':ID', $ideasID);
         $s->execute();
     } catch (PDOException $e) {
-        $error = 'Error removing idea from categories';
+        $error = 'Error removing idea from category';
         include "$_PATH[errorPath]";
         exit();
     }
 
-    //delete the idea department
     try {
         $sql = "DELETE FROM IdeaDepartment WHERE IdeaID= :ID";
         $s = $pdo->prepare($sql);
         $s->bindvalue(':ID', $ideasID);
         $s->execute();
     } catch (PDOException $e) {
-        $error = 'Error removing idea from categories';
+        $error = 'Error removing idea from department';
         include "$_PATH[errorPath]";
         exit();
     }
 
-    //delete the Comment
     try {
         $sql = "DELETE FROM Comment WHERE IdeaID= :ID";
         $s = $pdo->prepare($sql);
         $s->bindvalue(':ID', $ideasID);
         $s->execute();
     } catch (PDOException $e) {
-        $error = 'Error deleting idea';
+        $error = 'Error deleting idea from comment';
         include "$_PATH[errorPath]";
         exit();
     }
