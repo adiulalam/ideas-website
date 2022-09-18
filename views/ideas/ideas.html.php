2c4cd4a023e8e55c6589f91fd060111b483d4289
Adiul Alam Adil <adiulalam@gmail.com>
2022-09-18 16:51:48 +0100

    idea name changes

--- views/ideas/ideas.html.php
+++ views/ideas/ideas.html.php
@@ -6,7 +6,7 @@
   <script src="https://cdn.tailwindcss.com"></script>
   <script type="text/javascript" src="https://code.jquery.com/jquery-3.6.1.min.js"></script>
   <meta charset="utf-8">
-  <title>All Ideas</title>
+  <title>Ideal Ideas</title>
 </head>
 
 <?php include_once $_SERVER['DOCUMENT_ROOT'] . '/components/nav.html.php'; ?>
