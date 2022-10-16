73c5e0d4ba4ef652f60be1cdac218e7d4f8e90e4
Adiul Alam Adil <adiulalam@gmail.com>
2022-10-16 19:38:23 +0100

    readme

---
+++ example.environment.php
@@ -0,0 +1,13 @@
+<?php
+$_ENV["HOST"] = "DB IP Address";
+$_ENV["PORT"] = "DB Port";
+$_ENV["DATABASE"] = "DB Name";
+$_ENV["USERNAME"] = "DB Username";
+$_ENV["PASSWORD"] = "DB Password";
+
+
+$_ENV["EMAIL"] = "Gmail SMTP Email Address";
+$_ENV["EMAIL_PASSWORD"] = "Gmail App Password";
+
+
+$_ENV["CAPTCHA_SECRET_KEY"] = "Google Captcha Key";
