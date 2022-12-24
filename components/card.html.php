dbeb86cdcf9b269a2173d722fe3889043cd26694
Adiul Alam Adil <adiulalam@gmail.com>
2022-12-24 00:24:38 +0000

    delete css

--- components/card.html.php
+++ components/card.html.php
@@ -14,6 +14,8 @@ function mutationCheck($IdeaID, $totalIdeas)
             (in_array("Site Administrator", $_SESSION['authorRole']) ? userHasRole('Site Administrator') : false))
     ) {
         $checkTextSize = isMobileDevice() ? 'p-3 text-3xl' : 'mx-1 p-2 text-sm';
+        $buttonTextSize = isMobileDevice() ? 'text-3xl' : 'text-sm';
+        $promptTextSize = isMobileDevice() ? 'text-3xl' : 'text-lg';
         $mutationForm = "
         <form action='?' method='post' class=' float-right inline-flex items-center '>
             <input type='hidden' name='ID' value='$IdeaID'>
@@ -22,7 +24,7 @@ function mutationCheck($IdeaID, $totalIdeas)
         </form>
 
         <div id='$IdeaID' tabindex='-1' class='hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full'>
-            <div class='relative p-4 w-full max-w-md h-full md:h-auto'>
+            <div class='relative p-4 w-full max-w-2xl h-full md:h-auto'>
                 <div class='relative rounded-lg shadow bg-gray-700'>
                     <button type='button' class='absolute top-3 right-2.5 text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white' data-modal-toggle='$IdeaID'>
                         <svg aria-hidden='true' class='w-5 h-5' fill='currentColor' viewBox='0 0 20 20' xmlns='http://www.w3.org/2000/svg'><path fill-rule='evenodd' d='M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z' clip-rule='evenodd'></path></svg>
@@ -30,13 +32,13 @@ function mutationCheck($IdeaID, $totalIdeas)
                     </button>
                     <div class='p-6 text-center'>
                         <svg aria-hidden='true' class='mx-auto mb-4 w-14 h-14 text-gray-200' fill='none' stroke='currentColor' viewBox='0 0 24 24' xmlns='http://www.w3.org/2000/svg'><path stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z'></path></svg>
-                        <h3 class='mb-5 text-lg font-normal text-gray-400'>Are you sure you want to delete this Idea?</h3>
+                        <h3 class='mb-5 $promptTextSize font-normal text-gray-400'>Are you sure you want to delete this Idea?</h3>
                         <form action='?' method='post'>
                             <input type='hidden' name='ID' value='$IdeaID'>
-                            <button data-modal-toggle='$IdeaID' type='submit' name='action' value='Delete' class='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2'>
+                            <button data-modal-toggle='$IdeaID' type='submit' name='action' value='Delete' class='text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-800 font-medium rounded-lg $buttonTextSize inline-flex items-center px-5 py-2.5 text-center mr-2'>
                                 Yes, I'm sure
                             </button>
-                        <button data-modal-toggle='$IdeaID' type='button' class='focus:ring-4 focus:outline-none rounded-lg border text-sm font-medium px-5 py-2.5 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600'>No, cancel</button>
+                        <button data-modal-toggle='$IdeaID' type='button' class='focus:ring-4 focus:outline-none rounded-lg border $buttonTextSize font-medium px-5 py-2.5 focus:z-10 bg-gray-700 text-gray-300 border-gray-500 hover:text-white hover:bg-gray-600 focus:ring-gray-600'>No, cancel</button>
                         </form>
                     </div>
                 </div>
